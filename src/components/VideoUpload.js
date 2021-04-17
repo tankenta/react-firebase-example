954d930d4ac9183f54d8920e3ad96db0ccc25235
tankenta <hkenta112@gmail.com>
2021-04-17 23:54:47 +0900

    Add metadata handling using firestore

--- src/components/VideoUpload.js
+++ src/components/VideoUpload.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/storage';
+import 'firebase/firestore';
+import _ from 'lodash'
 
-const handleChange = (event, setVideo) => {
+const handleFileSelect = (event, setVideo) => {
   event.preventDefault();
   const video = event.target.files[0];
   setVideo(video);
@@ -13,16 +15,41 @@ const handleSubmit = (event, video) => {
   fileUpload(video);
 }
 
-const fileUpload = async video => {
+const fileUpload = async (video, setVideo) => {
   try {
     const userUid = firebase.auth().currentUser.uid;
     const filePath = `videos/${userUid}/${video.name}`;
     const videoStorageRef = firebase.storage().ref(filePath);
-    const fileSnapshot = await videoStorageRef.put(video);
+    const idToken = await firebase.auth().currentUser.getIdToken(true);
+    const metadataForStorage = {
+      customMetadata: { idToken }
+    };
+    const fileSnapshot = await videoStorageRef.put(video, metadataForStorage);
+
+    if (video.type === 'video/mp4') {
+      const downloadURL = await videoStorageRef.getDownloadURL();
+      const metadataForFirestore = {
+        ..._.omitBy(fileSnapshot.metadata, _.isEmpty),
+        downloadURL
+      };
+      saveVideoMetadata(metadataForFirestore);
+    }
+
     console.log(fileSnapshot);
+    if (fileSnapshot.state === 'success') {
+      setVideo(null);
+    } else {
+      alert('File upload failed');
+    }
   } catch (error) {
     console.log(error);
   }
+}
+
+const saveVideoMetadata = metadata => {
+  const userUid = firebase.auth().currentUser.uid;
+  const videoRef = firebase.firestore().doc(`users/${userUid}`).collection('videos').doc();
+  await videoRef.set({ ...metadata, uid: videoRef.id, merge: true });
 }
 
 const VideoUpload = () => {
@@ -31,7 +58,7 @@ const VideoUpload = () => {
   return (
     <form onSubmit={e => handleSubmit(e, video)}>
       <h2>Video Upload</h2>
-      <input type="file" accept="video/*" onChange={e => handleChange(e, setVideo)} />
+      <input type="file" accept="video/*" onChange={e => handleFileSelect(e, setVideo)} />
       <button type="submit">Upload Video</button>
     </form>
   );
