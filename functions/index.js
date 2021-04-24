f5f87e8c4b87db0638758ceb28474054377ef089
tankenta <hkenta112@gmail.com>
2021-04-25 02:23:24 +0900

    Add ametadata copy processing and split index.js into multiple files

--- functions/index.js
+++ functions/index.js
@@ -1,90 +1,29 @@
-const functions = require("firebase-functions");
-const path = require('path');
-const os = require('os');
-const fs = require('fs');
-const ffmpeg = require('fluent-ffmpeg');
-const ffmpegBin = require('@ffmpeg-installer/ffmpeg');
-const UUID = require('uuid-v4');
-const serviceAccount = require('./config/service_account.json');
-
-const { Storage } = require('@google-cloud/storage');
-const gcs = new Storage({ keyFilename: './config/service_account.json' });
-
-const admin = require('firebase-admin');
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: 'https://react-firebase-example-560cf.firebaseio.com',
-});
-admin.firestore().settings({ timestampsInSnapshots: true });
-
-function promisifyCommand(command) {
-  return new Promise((resolve, reject) => {
-    command.on('end', resolve).on('error', reject).run();
-  });
+/*
+* Triggered when the user uploads a video file, the video is transcoded to mp4.
+*/
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'transcodeVideo') {
+  exports.transcodeVideo = require('./transcodeVideo').transcodeVideo;
 }
 
-async function saveVideoMetadata(userToken, metadata) {
-  const decodedToken = await admin.auth().verifyIdToken(userToken);
-  const userUid = decodedToken.uid;
-  const videoRef = admin.firestore().doc(`users/${userUid}`).collection('videos').doc();
-  await videoRef.set({ ...metadata, uid: videoRef.id }, { merge: true });
+/*
+* Triggered when creating a new user, the user account is saved in Firestore.
+*/
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'saveUser') {
+  exports.saveUser = require('./saveUser').saveUser;
 }
 
-exports.transcodeVideo = functions.storage.object().onFinalize(async object => {
-  try {
-    const contentType = object.contentType;
+/*
+* Triggered when creating new video data, the video data is copied to Firestore.
+*/
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'onUsersVideoCreate') {
+  const copyVideoMetadata = require('./copyVideoMetadata');
+  exports.onUsersVideoCreate = copyVideoMetadata.onUsersVideoCreate;
+}
 
-    if (!contentType.includes('video') || contentType.endsWith('mp4')) {
-      console.log('quit execution!');
-      return;
-    }
-
-    const bucketName = object.bucket;
-    const bucket = gcs.bucket(bucketName);
-    const filePath = object.name;
-    const fileName = filePath.split('/').pop();
-    const tempFilePath = path.join(os.tmpdir(), fileName);
-    const videoFile = bucket.file(filePath);
-
-    const targetTempFileName = `${fileName.replace(/\.[^/.]+$/, '')}_output.mp4`;
-    const targetTempFilePath = path.join(os.tmpdir(), targetTempFileName);
-    const targetTranscodedFilePath = `transcoded-videos/${targetTempFileName}`;
-    const targetStorageFilePath = path.join(
-        path.dirname(targetTranscodedFilePath), targetTempFileName
-    );
-    await videoFile.download({ destination: tempFilePath });
-
-    const command = ffmpeg(tempFilePath)
-        .setFfmpegPath(ffmpegBin.path)
-        .format('mp4')
-        .output(targetTempFilePath);
-
-    await promisifyCommand(command);
-
-    const token = UUID();
-    await bucket.upload(targetTempFilePath, {
-        destination: targetStorageFilePath,
-        metadata: {
-          contentType: 'video/mp4',
-          metadata: {
-            firebaseStorageDownloadTokens: token,
-          },
-        },
-    });
-
-    const transcodedVideoFile = await bucket.file(targetStorageFilePath);
-    const metadata = await transcodedVideoFile.getMetadata();
-    const downloadURL = 'https://firebasestorage.googleapis.com/v0/b/' +
-        `${bucketName}/o/${encodeURIComponent(targetTranscodedFilePath)}?alt=media&token=${token}`;
-    const userToken = object.metadata.idToken;
-    await saveVideoMetadata(userToken, { ...metadata[0], downloadURL });
-
-    fs.unlinkSync(tempFilePath);
-    fs.unlinkSync(targetTempFilePath);
-
-    console.log('Transcode execution was finished!');
-  } catch (error) {
-    console.log(error);
-    return;
-  }
-});
+/*
+* Triggered when updating the video data, the video data is copied to Firestore.
+*/
+if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === 'onUsersVideoUpdate') {
+  const copyVideoMetadata = require('./copyVideoMetadata');
+  exports.onUsersVideoUpdate = copyVideoMetadata.onUsersVideoUpdate;
+}
